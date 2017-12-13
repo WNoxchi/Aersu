f7a0a1df3ab111f07d656ecae054096c5d42d2e3
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-12-13 16:14:43 -0500

    fixed path error

--- GLOC/combine_labels_script.py
+++ GLOC/combine_labels_script.py
@@ -5,7 +5,7 @@ import numpy as np
 from glob import glob
 
 # list all .csv's
-g = glob('*.csv')
+g = glob('data/*.csv')
 # sort indices by starting image index
 temp = sorted([[i,e.split('_')[1].split('-')[0]] for i,e in enumerate(g)], key = lambda x: int(x[1]))
 # sort csv list by index
