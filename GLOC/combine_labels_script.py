e5b1527c3dc0bf3c82b30735225eefaddea0fa54
Wayne Nixalo <wayne.noxchi@gmail.com>
2018-01-30 19:14:44 -0500

    changes to label comiber

--- GLOC/combine_labels_script.py
+++ GLOC/combine_labels_script.py
@@ -6,7 +6,8 @@ import numpy as np
 from glob import glob
 
 # list all .csv's
-g = glob('data/labels/*.csv')
+# g = glob('data/labels/*.csv')
+g = glob('data/interstage_labels-*.csv')
 # sort indices by starting image index
 temp = sorted([[i,e.split('_')[1].split('-')[0]] for i,e in enumerate(g)], key = lambda x: int(x[1]))
 # sort csv list by index
@@ -17,4 +18,9 @@ df = pd.read_csv(flist[0])
 for i in range(1, len(flist)):
     df = df.append(pd.read_csv(flist[i]))
 # save the new csv file
-df.to_csv('data/labels.csv', index=False)
+# df.to_csv('data/labels.csv', index=False)
+df.to_csv('data/interstage_labels.csv', index=False)
+
+
+# NOTE: the temp line makes a sorted list of files along w their original indices
+# NOTE: using sorted() and key: https://developers.google.com/edu/python/sorting
